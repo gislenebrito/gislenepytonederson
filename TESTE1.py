ae97caba4e661c6b66c9e9e6dc2e1bb34a9cd975
gislenebrito <britogislene54@gmail.com>
2024-09-25 16:21:21 -0300

    commit
    first commit

---
+++ TESTE1.py
@@ -0,0 +1,45 @@
+print("hello word!!!hello\n jose")
+print(2+3)
+print("dia","mes", "ano", sep="/")
+#sep é um separador que separa os valores de acordo com minha escolha,ou seja ponto virgula etc
+
+print(3.14)
+print(type(3.14))
+#type mostra em qual variavel se encaixa o valor dentro dela
+#int é a variavel de numero inteiro
+#float é a variavel de numero real
+
+print(42000)
+print(42,000)
+print(42.000)
+print(type(42000))
+print(type(42.000))
+print(type(42))
+print("10"+"10")
+print("10")
+print("3.14")
+print(type("3.14"))
+print(type('esse é um string'))
+print(type("e esse tambem é um string."))
+# os dois tipos de aspa servem para trasformar em texto escrito
+# pose se usar tres vezes as aspas para transformar em texto
+
+tipo= True
+print(type(tipo))
+mensagem = "jose matematico"
+n = 2021
+pi = 3.234
+print(mensagem)
+print(n)
+print(pi)
+print(81**0.5)
+#calcular a raiz quadrada de um numero
+
+p=65
+a=1.50
+
+
+print("primeiro exercicio")
+imc = p/a**2
+print("seu imc é ", imc)
+#calculo do imc
---
+++ gi.py
@@ -0,0 +1,53 @@
+
+
+# nome = input ("digite seu nome")
+# print ("o nome digitado foi: ",nome, ".")
+
+# salario = int(input("salario?"))
+# int(salario)
+# imposto = float(input("imposto"))
+# print(imposto)
+
+# sal = float(input("digite seu salario"))
+# print (sal)
+# print ("o salario digitado foi %f" % (sal))
+
+# print (" eu tenho um filho de %d anos, e outro de %d anos" % (10,23,25))
+
+# z = 1724/111
+
+# print("%. 2f" % z)
+# print ("%.4f" % z)
+
+# a = "Ederson"
+# b = "Costa"
+
+# print ("prezado, " +a+" "+b+" "+"Olá!")
+
+# print (10* "Ederson\n")
+
+# print("+" + 10 * "-" + "+")
+# print(("|" + " " * 10 + "|\n") * 5, end="")
+# print("+" + 10 * "-" + "+")
+
+# print ("+" + 10 *"+")
+# print(("|" + " " * 10 + "|\n") * 5, end="")
+# print ("+" + 10 *"+")
+
+# frase = "um triangulo de base igual a {0} e altura igual a {1} possui area igual a {2}".format(3,4,12, 55, 65)
+# print(frase)
+
+# 
+f= float(input("digite a temperatura: 12.45"))
+c= (5/9)* (f-32)
+print(f"a temperatura convertida para celsius é {c:.2f}")
+
+print ("boa tarde")
+
+
+
+
+
+
+
+
