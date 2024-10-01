99682692d3ac4eafbfba8852effec232d23fdc01
gislenebrito <britogislene54@gmail.com>
2024-10-01 17:33:59 -0300

    Create 10 listas.py
    python-listas 01/10/2024
    Ederson

---
+++ manipulaçao de intrings-funçao len 26/09/aula 01/10 listas.py
@@ -0,0 +1,21 @@
+#listas
+x= "Ederson"
+y=["ederson" , 18,20,34]
+
+lista1 = [10,20,30,40]
+lista2 = ["spam" , "bungee", "swallow" ]
+print (lista1, lista2)
+
+lista1 = ["oi" , 2.0,5, [10,20]]
+print (lista1)
+#len
+lista1 = ["oi", 2.0, 5, [10,20]]
+print (len(lista1))
+
+numeros = [17, 123, 87, 34, 66, 8398, 44]
+print(numeros[2])
+print(numeros[9-8])
+print(numeros[-2])
+print(numeros[-1])
+
+
