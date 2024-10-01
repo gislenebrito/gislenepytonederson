ad4e653131de3284401c2756de3454a837748f2b
gislenebrito <britogislene54@gmail.com>
2024-10-01 18:28:29 -0300

    exercicio 1
    cria a lista

--- manipulaçao de intrings-funçao len 26/09/aula 01/10 listas.py
+++ manipulaçao de intrings-funçao len 26/09/aula 01/10 listas.py
@@ -17,5 +17,29 @@ print(numeros[2])
 print(numeros[9-8])
 print(numeros[-2])
 print(numeros[-1])
+#quando tiver 2 elementos dentro da lista ou seja 2 colchetes imprime o primeiro e conta o outo dentro do mesmo elemento
 
+frutas = ["maça","laranja","banana","cereja"]
+print("maça" in frutas)
+print("pera" in frutas)
 
+frutas = ["maça","laranja","banana","cereja"]
+print([1,2] + [3,4])
+print(frutas + [6,7,8,9])
+
+print([]*4)
+print([1,2,["ola", "adeus"]]*2)
+
+a = [1,2,3,4,5,6,7,8,9]
+print (a)
+print(max(a))
+print(min(a))
+print(sum(a))
+
+#fatias de listas
+uma_lista = ['a', 'b', 'c', 'd', 'e', 'f']
+print(uma_lista[1:3])
+print(uma_lista[:4])
+print(uma_lista[3:])
+print(uma_lista[:])
+print(uma_lista[0:6])
---
+++ manipulaçao de intrings-funçao len 26/09/aula 01/exercicios.py
@@ -0,0 +1,7 @@
+a = [10,20,30,40,50,]
+
+print (a)
+print(max(a))
+print(min(a))
+print(len(a))
+print(sum(a))
